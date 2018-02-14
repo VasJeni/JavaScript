705dc5b6bf63fc34a742db8f2aacac33778eb4f7
VasJeni <vasilyev508@gmail.com>
2018-02-14 16:23:19 +0200

    upgrade index

---
+++ book/chaterTwo/function_bark.js
@@ -0,0 +1,11 @@
+function bark(name,weight) {
+    if (weight>20){
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + "says woof woof");
+    }
+}
+bark("rower", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("Lady", 17);
